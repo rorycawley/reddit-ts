5298c96c1671808bc310d259ac5820254500db71
rory cawley <rory.cawley@gmail.com>
2020-07-07 19:00:56 +0100

    refactored tests

---
+++ src/services/reddit/processPosts.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable camelcase */
+import formatDistance from 'date-fns/formatDistance';
+import { PostProps } from 'src/components/Root/Post';
+import { PostData } from './reddit';
+
+export const getCreated = (created_utc: number) =>
+  formatDistance(created_utc * 1000, Date.now(), {
+    addSuffix: true
+  });
+
+export const removeCRLF = (s?: string) =>
+  s ? s.replace(/(\r\n|\n|\r)/gm, ' ') : s;
+
+const defaultPostProps: PostProps = {
+  author: '',
+  id: '',
+  over18: false,
+  title: '',
+  created: ''
+};
+
+export const processPost = (post: PostData): PostProps => {
+  let processedPost: PostProps = defaultPostProps;
+
+  processedPost = { ...processedPost, id: post.id };
+  processedPost = { ...processedPost, author: post.author };
+  processedPost = { ...processedPost, title: post.title };
+  processedPost = { ...processedPost, over18: post.over_18 };
+  processedPost = { ...processedPost, created: getCreated(post.created_utc) };
+
+  if (post.selftext) {
+    processedPost = { ...processedPost, selftext: removeCRLF(post.selftext) };
+  }
+
+  if (
+    post.thumbnail &&
+    post.url_overridden_by_dest &&
+    post.thumbnail !== 'self' &&
+    !post.no_follow
+  ) {
+    processedPost = {
+      ...processedPost,
+      image: post.url_overridden_by_dest
+    };
+  }
+
+  if (
+    post.thumbnail &&
+    post.url_overridden_by_dest &&
+    post.thumbnail !== 'self' &&
+    post.no_follow
+  ) {
+    processedPost = {
+      ...processedPost,
+      image: post.thumbnail
+    };
+
+    processedPost = {
+      ...processedPost,
+      urltext: post.url_overridden_by_dest
+    };
+  }
+
+  return processedPost;
+};
--- tests/unit/processPost.test.ts
+++ tests/unit/processPost.test.ts
@@ -9,69 +9,7 @@ import {
 } from 'tests/data';
 
 import { PostProps } from 'components/Root/Post';
-
-import { PostData } from 'src/services/reddit/reddit';
-import formatDistance from 'date-fns/formatDistance';
-
-const getCreated = (created_utc: number) =>
-  formatDistance(created_utc * 1000, Date.now(), {
-    addSuffix: true
-  });
-
-const defaultPostProps: PostProps = {
-  author: '',
-  id: '',
-  over18: false,
-  title: '',
-  created: ''
-};
-
-const removeCRLF = (s?: string) => (s ? s.replace(/(\r\n|\n|\r)/gm, ' ') : s);
-
-const processPost = (post: PostData): PostProps => {
-  let processedPost: PostProps = defaultPostProps;
-
-  processedPost = { ...processedPost, id: post.id };
-  processedPost = { ...processedPost, author: post.author };
-  processedPost = { ...processedPost, title: post.title };
-  processedPost = { ...processedPost, over18: post.over_18 };
-  processedPost = { ...processedPost, created: getCreated(post.created_utc) };
-
-  if (post.selftext) {
-    processedPost = { ...processedPost, selftext: removeCRLF(post.selftext) };
-  }
-
-  if (
-    post.thumbnail &&
-    post.url_overridden_by_dest &&
-    post.thumbnail !== 'self' &&
-    !post.no_follow
-  ) {
-    processedPost = {
-      ...processedPost,
-      image: post.url_overridden_by_dest
-    };
-  }
-
-  if (
-    post.thumbnail &&
-    post.url_overridden_by_dest &&
-    post.thumbnail !== 'self' &&
-    post.no_follow
-  ) {
-    processedPost = {
-      ...processedPost,
-      image: post.thumbnail
-    };
-
-    processedPost = {
-      ...processedPost,
-      urltext: post.url_overridden_by_dest
-    };
-  }
-
-  return processedPost;
-};
+import { removeCRLF, processPost } from 'src/services/reddit/processPosts';
 
 const checkPost = (processedPostData: PostProps, comparator: PostProps) => {
   expect(processedPostData.id).toBe(comparator.id);
