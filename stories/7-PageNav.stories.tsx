19c51d74760932c931300d8a21d033da11b626d7
rory cawley <rory.cawley@gmail.com>
2020-07-08 10:05:43 +0100

    PageNav story updating

--- stories/7-PageNav.stories.tsx
+++ stories/7-PageNav.stories.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { action } from '@storybook/addon-actions';
 
 // import Post, { PostProps } from '../src/components/Root/PostList/Post';
 import PageNav from '../src/components/Root/PostList/PageNav';
@@ -8,9 +9,18 @@ export default {
   component: PageNav
 };
 
-export const Default: FC = () => <PageNav />;
-export const BackEnabled: FC = () => <PageNav backDisabled={false} />;
-export const NextEnabled: FC = () => <PageNav nextDisabled={false} />;
+const actionsData = {
+  clickBack: action('clickBack'),
+  clickNext: action('clickNext')
+};
+
+export const Default: FC = () => <PageNav {...actionsData} />;
+export const BackEnabled: FC = () => (
+  <PageNav backDisabled={false} {...actionsData} />
+);
+export const NextEnabled: FC = () => (
+  <PageNav nextDisabled={false} {...actionsData} />
+);
 export const BackNextEnabled: FC = () => (
-  <PageNav backDisabled={false} nextDisabled={false} />
+  <PageNav backDisabled={false} nextDisabled={false} {...actionsData} />
 );
