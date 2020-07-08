35c0426574e857253c730b4bc3307cd988a64120
rory cawley <rory.cawley@gmail.com>
2020-07-08 07:33:34 +0100

    PostList to become story

---
+++ stories/3-PostList.stories.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+// import Post, { PostProps } from '../src/components/Root/PostList/Post';
+import PostList, { PostListProps } from '../src/components/Root/PostList';
+import { postListWithFourPosts } from '../tests/data/postLists';
+
+export default {
+  title: 'PostList',
+  component: PostList
+};
+
+export const Default: FC = () => <PostList />;
+export const Loading: FC = () => <PostList loading={true} />;
+export const Empty: FC = () => <PostList posts={[]} />;
+export const FourPosts: FC = () => (
+  <PostList posts={postListWithFourPosts.posts} />
+);
+// export const Loading: FC = () => <PostList {...postListLoading} />;
+// export const PostListEmptyList: FC = () => (
+//          <PostList title='Loading2' {...postListEmptyList} />
+//        );
+// export const PostListWithFourPosts: FC = () => (
+//   <PostList {...postListWithFourPosts} />
+// );
+
+// export default { loading: false, posts: [] };
