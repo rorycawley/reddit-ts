243fc2cb1d0f5e9211f0347ec59228935b4e3958
rory cawley <rory.cawley@gmail.com>
2020-07-08 23:13:33 +0100

    minor update to fix yarn start

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Root: React.FC = () => {
+const Root: FC = () => {
   return <div>Root</div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Root from 'components/Root';
+import Root from './components/Root';
 
 render(<Root />, document.getElementById('app'));
