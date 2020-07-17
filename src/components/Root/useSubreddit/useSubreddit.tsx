dc9ff0d046dd9c063b76d0668337ba8060a64f78
rory cawley <rory.cawley@gmail.com>
2020-07-17 12:28:18 +0100

    added logging for context reducer

--- src/components/Root/useSubreddit/useSubreddit.tsx
+++ src/components/Root/useSubreddit/useSubreddit.tsx
@@ -12,6 +12,10 @@ type Action = { type: 'SET_SUBREDDIT'; payload: string };
 export const reducer = (state: AppState, action: Action): AppState => {
   switch (action.type) {
     case 'SET_SUBREDDIT':
+      console.info(
+        `%cCTX - SET_SUBREDDIT: "${action.payload}"`,
+        'font-weight: bold;font-family:sans-serif; color: lime; font-size: 12px'
+      );
       return { payload: action.payload };
     default:
       throw new Error();
