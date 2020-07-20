8aa43f98a95f4f4ee115d7929790cd784f2ba184
rory cawley <rory.cawley@gmail.com>
2020-07-20 18:24:21 +0100

    test working for SearchTest, change search from reactjs to ireland

---
+++ tests/data/subreddits.ts
@@ -0,0 +1,65 @@
+export const reactjsSubreddits = {
+  subreddits: [
+    {
+      numSubscribers: 200191,
+      name: 'reactjs',
+      allowedPostTypes: {
+        images: false,
+        text: true,
+        videos: true,
+        links: true,
+        spoilers: true
+      },
+      id: 't5_2zldd',
+      primaryColor: '#014980',
+      communityIcon:
+        'https://styles.redditmedia.com/t5_2zldd/styles/communityIcon_fbblpo38vy941.png',
+      icon: ''
+    },
+    {
+      numSubscribers: 1154,
+      name: 'ReactJSLearn',
+      allowedPostTypes: {
+        images: true,
+        text: true,
+        videos: true,
+        links: true,
+        spoilers: true
+      },
+      id: 't5_3iadr',
+      primaryColor: '',
+      communityIcon: '',
+      icon: ''
+    },
+    {
+      numSubscribers: 538,
+      name: 'reactjsdevelopers',
+      allowedPostTypes: {
+        images: true,
+        text: true,
+        videos: true,
+        links: true,
+        spoilers: true
+      },
+      id: 't5_3e9j3',
+      primaryColor: '',
+      communityIcon: '',
+      icon: ''
+    },
+    {
+      numSubscribers: 1750,
+      name: 'reactjs_beginners',
+      allowedPostTypes: {
+        images: true,
+        text: true,
+        videos: true,
+        links: true,
+        spoilers: true
+      },
+      id: 't5_3c0ga',
+      primaryColor: '',
+      communityIcon: '',
+      icon: ''
+    }
+  ]
+};
--- tests/unit/components/SearchBar.test.tsx
+++ tests/unit/components/SearchBar.test.tsx
@@ -90,4 +90,115 @@ describe('SearchBar', () => {
     // console.log(items);
     // screen.debug();
   });
+
+  it('does a search for reduxjs and then to ireland populates the drowdown with the results', async () => {
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+
+    const input = screen.getByLabelText('Search for subreddits...');
+    fireEvent.change(input, {
+      target: {
+        value: 'reactjs'
+      }
+    });
+
+    // check the input box is set to 'reactjs'
+    expect(screen.getByRole('textbox')).toHaveValue('reactjs');
+
+    await expectRedux(store as StoreWithSpy<any, any>)
+      .toHaveState()
+      .matching({
+        subreddits: {
+          subreddits: []
+        },
+        ui: { loading: true }
+      });
+
+    // screen.debug();
+    expect(screen.getByText('Loading…')).toBeInTheDocument();
+
+    await expectRedux(store as StoreWithSpy<any, any>)
+      .toDispatchAnAction()
+      .matching({
+        type: '[Subreddits] SET_SUBREDDITS',
+        payload: {
+          subreddits: [
+            'reactjs',
+            'ReactJSLearn',
+            'reactjsdevelopers',
+            'reactjs_beginners'
+          ]
+        },
+        meta: {
+          dataNormalized: true,
+          feature: '[Subreddits]'
+        }
+      });
+
+    await expectRedux(store as StoreWithSpy<any, any>)
+      .toHaveState()
+      .matching({
+        subreddits: {
+          subreddits: [
+            'reactjs',
+            'ReactJSLearn',
+            'reactjsdevelopers',
+            'reactjs_beginners'
+          ]
+        },
+        ui: {
+          loading: false
+        }
+      });
+
+    expect(screen.queryByText('Loading…')).toBeNull();
+
+    expect(screen.getByRole('listbox').children).toHaveLength(4);
+    expect(screen.getByText('ReactJSLearn')).toBeInTheDocument();
+    expect(screen.getByText('reactjsdevelopers')).toBeInTheDocument();
+    expect(screen.getByText('reactjs_beginners')).toBeInTheDocument();
+    // console.log(items);
+    // screen.debug();
+
+    // change to search for ireland
+    fireEvent.change(input, {
+      target: {
+        value: 'ireland'
+      }
+    });
+
+    // check the input box is set to 'reactjs'
+    expect(screen.getByRole('textbox')).toHaveValue('ireland');
+
+    await expectRedux(store as StoreWithSpy<any, any>)
+      .toHaveState()
+      .matching({
+        subreddits: {
+          subreddits: [
+            'reactjs',
+            'ReactJSLearn',
+            'reactjsdevelopers',
+            'reactjs_beginners'
+          ]
+        },
+        ui: {
+          loading: true
+        }
+      });
+
+    // // screen.debug();
+    expect(screen.getByText('Loading…')).toBeInTheDocument();
+    expect(await screen.findByText(/IrelandGaming/i)).toBeTruthy();
+
+    expect(screen.queryByText('Loading…')).toBeNull();
+
+    expect(screen.getByRole('listbox').children).toHaveLength(8);
+    expect(screen.getByText('ireland')).toBeInTheDocument();
+    expect(screen.getByText('IrelandGaming')).toBeInTheDocument();
+    expect(screen.getByText('IrelandBaldwin')).toBeInTheDocument();
+    expect(screen.getByText('IrelandPics')).toBeInTheDocument();
+    expect(screen.getByText('IrelandonReddit')).toBeInTheDocument();
+    expect(screen.getByText('IrelandAssistance')).toBeInTheDocument();
+    expect(screen.getByText('Irelandgonetrolling')).toBeInTheDocument();
+    expect(screen.getByText('IrelandJournalism')).toBeInTheDocument();
+  });
 });
