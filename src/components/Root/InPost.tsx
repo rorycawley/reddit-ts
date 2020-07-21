34b06c9f6478617f07dd8da8a441673a83c2ba39
rory cawley <rory.cawley@gmail.com>
2020-07-21 12:54:56 +0100

    more styling of post

--- src/components/Root/InPost.tsx
+++ src/components/Root/InPost.tsx
@@ -2,48 +2,58 @@ import Avatar from '@material-ui/core/Avatar';
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const PostHeader = styled.div`
-  display: flex;
-  align-items: center;
+const Post = styled.div`
+  background-color: white;
+  max-width: 500px;
+  border: 1px solid lightgrey;
 `;
 
-const PostImage = styled.img`
-  object-fit: contain;
-  width: 100%;
-`;
-
-const StyledAvatar = styled(Avatar)`
+const PostAvatar = styled(Avatar)`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
   background-color: #6772e5;
   color: #fff;
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  padding: 7px 14px;
   margin-right: 10px;
+  padding: 7px 14px;
   &:hover {
     background-color: #5469d4;
   }
 `;
 
-const UserComment = styled.strong`
+const PostHeader = styled.div`
+  align-items: center;
+  display: flex;
+  padding: 20px;
+`;
+
+const PostImage = styled.img`
+  object-fit: contain;
+  width: 100%;
+  border-top: 1px solid lightgrey;
+  border-bottom: 1px solid lightgrey;
+`;
+
+const PostText = styled.strong`
   font-weight: normal;
+  padding: 20px;
 `;
 
 const UserNameText = styled.strong``;
 
 const InPost: FC = () => {
   return (
-    <div>
+    <Post>
       <PostHeader>
-        <StyledAvatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+        <PostAvatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
         <h3>Username</h3>
       </PostHeader>
       <PostImage
         alt={`User Avatar`}
         src={`https://www.pngrepo.com/png/221028/180/user-avatar.png`}
       />
-      <UserComment>
+      <PostText>
         <UserNameText>Username:</UserNameText> wow on day three
-      </UserComment>
-    </div>
+      </PostText>
+    </Post>
   );
 };
 
