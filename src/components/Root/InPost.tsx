0e5cc79ec7d483f4c5882243f98b9ff648860e8f
rory cawley <rory.cawley@gmail.com>
2020-07-21 11:49:50 +0100

    add useravatar to post

--- src/components/Root/InPost.tsx
+++ src/components/Root/InPost.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const UserAvatar = styled.img`
+  object-fit: contain;
+`;
 
 const InPost: FC = () => {
   return (
     <div>
       <h3>Username</h3>
+      <UserAvatar
+        alt={`User Avatar`}
+        src={`https://www.pngrepo.com/png/221028/180/user-avatar.png`}
+      />
     </div>
   );
 };
